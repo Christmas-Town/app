081c542a9da055a6d79458c4ae178fe1c4371e83
Andrew <mail@andrewyg.net>
2017-11-03 23:55:17 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.status(200).send("<h1>Please wait! The site is still developing!</h1>");
 });
+
+app.get('/ascxz', express.static("ascxz"));
 
 app.listen(process.env.PORT || 5118);
