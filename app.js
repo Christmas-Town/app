41f24429f3a2ca13822e68ece0ab623b48fcb69c
Andrew <mail@andrewyg.net>
2017-11-04 00:36:49 +0800

    update

--- app.js
+++ app.js
@@ -5,6 +5,4 @@ app.get('/', function(req, res) {
   res.status(200).send("<h1>Please wait! The site is still developing!</h1>");
 });
 
-app.use('/ascxz', express.static("./ascxz"));
-
 app.listen(process.env.PORT || 5118);
