77418f5468d4c2b905e2aaf19f8c42a2fa942fc6
Andrew <mail@andrewyg.net>
2017-11-04 19:18:20 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
   res.status(200).send("<h1>Please wait! The site is still developing!</h1>");
 });
+
+app.get('2tothepowerof', function(req, res) {
+	res.render("test");
+})
 
 app.listen(process.env.PORT || 5118);
